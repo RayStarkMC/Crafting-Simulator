34cec5aa31b3d91095c087463eb7df730fe22006
RayStarkMC <raystarkmc@gmail.com>
2024-06-21 11:56:28 +0900

    http4s環境構築

--- api/build.sbt
+++ api/build.sbt
@@ -1,11 +1,21 @@
-import sbt.Keys.mainClass
+val http4sVersion = "0.23.27"
 
 ThisBuild / organization := "net.raystarkmc"
 ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "3.4.2"
+ThisBuild / scalacOptions ++= Seq(
+  "-Wnonunit-statement",
+  "-Yexplicit-nulls",
+  "-source:future"
+)
 
 lazy val root = (project in file("."))
   .settings(
-    name := "crafting-simulator-api",
-    Compile / run / mainClass := Some("net.raystarkmc.craftingsimulator.main")
+    name := "api",
+    libraryDependencies ++= Seq(
+      "org.http4s" %% "http4s-ember-client" % http4sVersion,
+      "org.http4s" %% "http4s-ember-server" % http4sVersion,
+      "org.http4s" %% "http4s-dsl" % http4sVersion,
+    ),
+    Compile / run / fork := true,
   )
--- api/src/main/scala/net/raystarkmc/craftingsimulator/Main.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/Main.scala
@@ -1,8 +1,30 @@
 package net.raystarkmc.craftingsimulator
 
-@main
-def main(): Unit = {
-  println("hello world")
+import cats.effect.{ExitCode, IO, IOApp}
+import com.comcast.ip4s.{ipv4, port}
+import org.http4s.{HttpRoutes, MediaType}
+import org.http4s.dsl.io.*
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.headers.`Content-Type`
+
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.SECONDS
+import scala.concurrent.duration.Duration
+
+val routes = HttpRoutes.of[IO] {
+  case GET -> Root / "hello" => Ok("Hello World!", `Content-Type`(MediaType.application.json))
 }
 
-
+object Main extends IOApp.Simple {
+  def run: IO[Unit] = {
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(routes.orNotFound)
+      .withShutdownTimeout(Duration(1, SECONDS))
+      .build
+      .useForever
+      .as(ExitCode.Success)
+  }
+}
