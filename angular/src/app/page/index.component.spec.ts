06d917b52881623e7f01ba3ff274f9e2096df682
RayStarkMC <raystarkmc@gmail.com>
2024-12-16 02:00:19 +0900

    テストがパスするように修正

--- angular/src/app/page/index.component.spec.ts
+++ angular/src/app/page/index.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IndexComponent } from './index.component';
+import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClientTesting} from "@angular/common/http/testing";
 
 describe('IndexComponent', () => {
   let component: IndexComponent;
@@ -8,7 +10,11 @@ describe('IndexComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [IndexComponent]
+      imports: [IndexComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     })
     .compileComponents();
 
