ce07bcc9309633b79b8a09116724d4bfeaf34f68
RayStarkMC <raystarkmc@gmail.com>
2024-12-19 02:29:22 +0900

    mapK定義

--- api/src/main/scala/net/raystarkmc/craftingsimulator/domain/item/ItemRepository.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/domain/item/ItemRepository.scala
@@ -1,5 +1,10 @@
 package net.raystarkmc.craftingsimulator.domain.item
 
+import cats.~>
+
 trait ItemRepository[F[_]]:
   def resolveById(itemId: ItemId): F[Option[Item]]
   def save(item: Item): F[Unit]
+  def mapK[G[_]](f: F ~> G): ItemRepository[G] = new ItemRepository[G]:
+    override def resolveById(itemId: ItemId): G[Option[Item]] = f(ItemRepository.this.resolveById(itemId))
+    override def save(item: Item): G[Unit] = f(ItemRepository.this.save(item))
