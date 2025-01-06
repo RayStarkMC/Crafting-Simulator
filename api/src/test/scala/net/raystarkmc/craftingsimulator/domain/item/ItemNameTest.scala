25dadc58ed3070842edafed44d7d423587d23bed
RayStarkMC <raystarkmc@gmail.com>
2025-01-07 00:43:54 +0900

    eqvを使う形でテストを修正

--- api/src/test/scala/net/raystarkmc/craftingsimulator/domain/item/ItemNameTest.scala
+++ api/src/test/scala/net/raystarkmc/craftingsimulator/domain/item/ItemNameTest.scala
@@ -1,53 +1,36 @@
 package net.raystarkmc.craftingsimulator.domain.item
 
+import cats.instances.all.given
+import cats.syntax.all.given
 import org.scalatest.freespec.AnyFreeSpec
 
 class ItemNameTest extends AnyFreeSpec:
-  "制御文字が含まれる場合失敗" in {
-    assertResult(
-      Left(ItemName.Error.ContainsControlCharacter)
-    ) {
-      ItemName.either("a\t")
-    }
-  }
+  "制御文字が含まれる場合失敗" in:
+    val expected = Left(ItemName.Error.ContainsControlCharacter).withRight[ItemName]
+    val actual = ItemName.either("a\t")
+    assert(expected eqv actual)
 
-  "空文字列の場合失敗" in {
-    assertResult(
-      Left(ItemName.Error.IsBlank)
-    ) {
-      ItemName.either("")
-    }
-  }
+  "空文字列の場合失敗" in:
+    val expected = Left(ItemName.Error.IsBlank).withRight[ItemName]
+    val actual = ItemName.either("")
+    assert(expected eqv actual)
 
-  "空白のみの場合失敗" in {
-    assertResult(
-      Left(ItemName.Error.IsBlank)
-    ) {
-      ItemName.either(" ")
-    }
-  }
+  "空白のみの場合失敗" in:
+    val expected = Left(ItemName.Error.IsBlank).withRight[ItemName]
+    val actual = ItemName.either(" ")
+    assert(expected eqv actual)
 
-  "全角空白のみの場合失敗" in {
-    assertResult(
-      Left(ItemName.Error.IsBlank)
-    ) {
-      ItemName.either("　")
-    }
-  }
+  "全角空白のみの場合失敗" in:
+    val expected = Left(ItemName.Error.IsBlank).withRight[ItemName]
+    val actual = ItemName.either("　")
+    assert(expected eqv actual)
 
+  "日本語で生成" in:
+    val expected = Right("サンプルアイテム").withLeft[ItemName.Error]
+    val actual = ItemName.either("サンプルアイテム").map(_.value)
+    assert(expected eqv actual)
 
-  "日本語で生成" in {
-    assertResult(
-      Right("サンプルアイテム").withLeft[ItemName.Error]
-    ) {
-      ItemName.either("サンプルアイテム")
-    }
-  }
-
-  "空文字列を含んだ日本語で生成" in {
-    assertResult(
-      Right("サンプル アイテム").withLeft[ItemName.Error]
-    ) {
-      ItemName.either("サンプル アイテム")
-    }
-  }
+  "空文字列を含んだ日本語で生成" in:
+    val expected = Right("サンプル アイテム").withLeft[ItemName.Error]
+    val actual = ItemName.either("サンプル アイテム").map(_.value)
+    assert(expected eqv actual)
