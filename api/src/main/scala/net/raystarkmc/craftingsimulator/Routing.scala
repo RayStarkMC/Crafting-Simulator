c966b6bdb1a819b32c831e4f1c6fc4f4e969bc30
RayStarkMC <raystarkmc@gmail.com>
2025-01-14 02:33:20 +0900

    不要エンドポイントを削除

--- api/src/main/scala/net/raystarkmc/craftingsimulator/Routing.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/Routing.scala
@@ -2,7 +2,6 @@ package net.raystarkmc.craftingsimulator
 
 import cats.effect.{IO, Sync}
 import cats.syntax.all.given
-import io.circe.generic.auto.given
 import io.circe.syntax.given
 import org.http4s.circe.*
 import org.http4s.dsl.io.*
@@ -10,19 +9,10 @@ import org.http4s.server.middleware.{ErrorAction, ErrorHandling}
 import org.http4s.{HttpRoutes, Request, Response}
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
-import doobie.*
-import doobie.implicits.given
-import net.raystarkmc.craftingsimulator.port.db.doobie.postgres.xa
 
 given logger[F[_]: Sync]: Logger[F] = Slf4jLogger.getLogger[F]
 
-case class Hello(message: String)
-
 val app: HttpRoutes[IO] = HttpRoutes.of[IO] {
-  case req @ POST -> Root / "api" / "hello" => hello(req)
-  case req @ GET -> Root / "api" / "error" => throw new RuntimeException("wow!")
-  case req @ GET -> Root / "api" / "dbtest" => dbtest(req)
-
   case req @ POST -> Root / "api" / "items" => postItems(req)
 }
 
@@ -43,31 +33,9 @@ val withErrorLogging: HttpRoutes[IO] = ErrorHandling.Recover.total(
   )
 )
 
-def hello(req: Request[IO]): IO[Response[IO]] = {
-  for {
-    body <- req.decodeJson[Hello]
-    res <- Ok(body.asJson)
-  } yield {
-    res
-  }
-}
-
-case class SqlResponse(value: String)
-
-def dbtest(req: Request[IO]): IO[Response[IO]] = {
-  val program = sql"select current_user".query[String].unique
-
-  for {
-    i <- program.transact(xa[IO])
-    res <- Ok(SqlResponse(i).asJson)
-  } yield {
-    res
-  }
-}
-
 def postItems(req: Request[IO]): IO[Response[IO]] = {
-  import net.raystarkmc.craftingsimulator.usecase.command.RegisterItemCommandHandler
   import net.raystarkmc.craftingsimulator.port.db.doobie.postgres.PGItemRepository.given
+  import net.raystarkmc.craftingsimulator.usecase.command.RegisterItemCommandHandler
 
   val commandHandler = summon[RegisterItemCommandHandler[IO]]
 
