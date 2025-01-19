0fbea4fef97b25cdd28af3fbae6eaf9a9222924c
RayStarkMC <raystarkmc@gmail.com>
2025-01-19 22:27:13 +0900

    PG実装定義

---
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/port/db/doobie/postgres/queryhandler/PGGetAllItemsQueryHandler.scala
@@ -0,0 +1,45 @@
+package net.raystarkmc.craftingsimulator.port.db.doobie.postgres.queryhandler
+
+import cats.effect.IO
+import net.raystarkmc.craftingsimulator.usecase.query.GetAllItemsQueryHandler
+import net.raystarkmc.craftingsimulator.usecase.query.GetAllItemsQueryHandler.*
+import net.raystarkmc.craftingsimulator.port.db.doobie.postgres.table.ItemTableRecord
+import net.raystarkmc.craftingsimulator.port.db.doobie.postgres.xa
+import doobie.*
+import doobie.implicits.given
+import doobie.postgres.implicits.given
+
+trait PGGetAllItemsQueryHandler extends GetAllItemsQueryHandler[IO]:
+  def run(): IO[AllItems] =
+    val query =
+      sql"""
+        select
+          item.id, item.name
+        from
+          item
+        order by
+          item.name,
+          item.id
+      """
+        .query[ItemTableRecord]
+        .to[Seq]
+    
+    for {
+      records <- query.transact[IO](xa)
+    } yield {
+      AllItems(
+        list = records.map { record =>
+          Item(
+            id = record.id.toString(),
+            name = record.name
+          )
+        }
+      )
+    }
+
+object PGGetAllItemsQueryHandler
+
+trait PGGetAllItemsQueryHandlerGivens:
+  given GetAllItemsQueryHandler[IO] =
+    object handler extends PGGetAllItemsQueryHandler
+    handler
