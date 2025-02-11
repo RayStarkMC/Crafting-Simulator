2ce4fe4751c4b3339a61b21e98af66177a80c6e5
RayStarkMC <raystarkmc@gmail.com>
2025-02-12 08:00:02 +0900

    Angular-Material追加

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -2,11 +2,12 @@ import {provideRouter} from "@angular/router";
 import {ApplicationConfig, provideZoneChangeDetection} from "@angular/core";
 import {routes} from "./app.routes";
 import {provideHttpClient} from "@angular/common/http";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), provideAnimationsAsync(),
   ]
 };
