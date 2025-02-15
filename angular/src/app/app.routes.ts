64b70b70dda27675b3e985fdfed7c7daa119be36
RayStarkMC <raystarkmc@gmail.com>
2025-02-16 07:09:20 +0900

    Angular Material導入

--- angular/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from "@angular/router";
-import {IndexComponent} from "./page/index.component";
-
-export const routes: Routes = [
-  {
-    path: "",
-    component: IndexComponent
-  }
-];
---
+++ angular/src/component/page/index/index.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {LayoutComponent} from "../../shared/layout/layout.component";
+
+@Component({
+  selector: 'index',
+    imports: [
+        LayoutComponent
+    ],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.css'
+})
+export class IndexComponent {
+
+}
