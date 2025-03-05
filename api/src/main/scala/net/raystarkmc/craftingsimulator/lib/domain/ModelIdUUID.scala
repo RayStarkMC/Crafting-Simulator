662bce64ad1fa0422518d2e66e8190b6622a57a3
RayStarkMC <raystarkmc@gmail.com>
2025-03-06 04:33:15 +0900

    asInstanceOf削除

--- api/src/main/scala/net/raystarkmc/craftingsimulator/lib/domain/ModelIdUUID.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/lib/domain/ModelIdUUID.scala
@@ -9,7 +9,7 @@ import java.util.UUID
 
 opaque type ModelIdUUID[C] <: UUID = UUID :| Pure
 
-private inline def wrapFModelIdUUID[F[_], C](uuidF: F[UUID]): F[ModelIdUUID[C]] = uuidF.asInstanceOf
+private inline def wrapFModelIdUUID[F[_], C](uuidF: F[UUID]): F[ModelIdUUID[C]] = uuidF.assumeAll // Pure制約のため
 
 trait ModelIdUUIDTypeOps[C] extends RefinedTypeOps[UUID, Pure, ModelIdUUID[C]]:
   def generate[F[_]: UUIDGen]: F[ModelIdUUID[C]] =
