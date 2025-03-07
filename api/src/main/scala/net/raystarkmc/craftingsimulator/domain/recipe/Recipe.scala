75110e89c02b9b9c3c88836e771c28bba8732984
RayStarkMC <raystarkmc@gmail.com>
2025-03-08 01:56:22 +0900

    Recipe定義

--- api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/Recipe.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/Recipe.scala
@@ -1,9 +1,14 @@
 package net.raystarkmc.craftingsimulator.domain.recipe
 
+import cats.{Eq, Hash}
+import cats.derived.*
+import io.github.iltotore.iron.*
+import io.github.iltotore.iron.cats.{*, given}
+import io.github.iltotore.iron.constraint.all.*
 import net.raystarkmc.craftingsimulator.domain.item.{*, given}
 import net.raystarkmc.craftingsimulator.lib.domain.*
 
-private sealed trait RecipeContext
+sealed trait RecipeContext
 
 type RecipeId = ModelIdUUID[RecipeContext]
 object RecipeId extends ModelIdUUIDTypeOps[RecipeContext]
@@ -13,15 +18,20 @@ type RecipeName = ModelName[RecipeContext]
 object RecipeName extends ModelNameTypeOps[RecipeContext]
 import RecipeName.given
 
-case class RecipeInput(
-    items: Seq[ItemId]
-)
+opaque type ItemCount = Long :| Greater[0]
+object ItemCount extends RefinedTypeOps[Long, Greater[0], ItemCount]
 
-case class RecipeOutput(
-    items: Seq[ItemId]
-)
+opaque type RecipeInput = Seq[ItemCount] :| Pure
+object RecipeInput extends RefinedTypeOps[Seq[ItemCount], Pure, RecipeInput]
 
-case class Recipe(
+opaque type RecipeOutput = Seq[ItemCount] :| Pure
+object RecipeOutput extends RefinedTypeOps[Seq[ItemCount], Pure, RecipeOutput]
+
+case class RecipeData(
     id: RecipeId,
-    name: RecipeName
-)
+    name: RecipeName,
+    inputs: RecipeInput,
+    outputs: RecipeOutput
+) derives Eq, Hash
+opaque type Recipe = RecipeData :| Pure
+object Recipe extends RefinedTypeOps[RecipeData, Pure, Recipe]
