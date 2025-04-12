612eea10bfc0e8b0ad1bc0e6919266c3c03b0f5a
RayStarkMC <raystarkmc@gmail.com>
2025-04-12 16:28:31 +0900

    DeleteRecipeCommandHandler定義

---
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/usecase/command/recipe/DeleteRecipeCommandHandler.scala
@@ -0,0 +1,45 @@
+package net.raystarkmc.craftingsimulator.usecase.command.recipe
+
+import cats.derived.*
+import cats.effect.std.UUIDGen
+import cats.instances.all.given
+import cats.syntax.all.given
+import cats.{Applicative, Hash, Monad, Show}
+import io.github.iltotore.iron.*
+import net.raystarkmc.craftingsimulator.domain.recipe.{
+  RecipeId,
+  RecipeRepository
+}
+import net.raystarkmc.craftingsimulator.usecase.command.DeleteItemCommandHandler.{
+  Command,
+  Output
+}
+
+import java.util.UUID
+
+trait DeleteRecipeCommandHandler[F[_]]:
+  def run(command: Command): F[Output]
+
+object DeleteRecipeCommandHandler extends DeleteRecipeCommandHandlerGivens:
+  case class Command(id: UUID) derives Hash, Show
+  case class Output() derives Hash, Show
+
+trait DeleteRecipeCommandHandlerGivens:
+  given [F[_]: {Monad, UUIDGen, RecipeRepository}]
+    => DeleteRecipeCommandHandler[F] =
+    object instance extends DeleteRecipeCommandHandler[F]:
+      private val recipeRepository = summon[RecipeRepository[F]]
+
+      def run(
+          command: Command
+      ): F[Output] =
+        val recipeId = RecipeId(command.id)
+        for {
+          recipeOpt <- recipeRepository.resolveById(recipeId)
+          _ <- recipeOpt.fold {
+            Applicative[F].unit
+          } { item =>
+            recipeRepository.delete(item)
+          }
+        } yield Output()
+    instance
