584cd0938ecc01b7f4a13822e0ce9a71201ff57f
RayStarkMC <raystarkmc@gmail.com>
2025-04-13 16:18:35 +0900

    Iron解除

--- api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/RecipeInput.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/RecipeInput.scala
@@ -1,7 +1,21 @@
 package net.raystarkmc.craftingsimulator.domain.recipe
 
-import io.github.iltotore.iron.*
+import cats.*
+import cats.implicits.*
+import net.raystarkmc.craftingsimulator.domain.recipe.RecipeInput.*
 
-type RecipeInput = RecipeInput.T
-object RecipeInput
-  extends RefinedType[Seq[ItemWithCount], Pure]
+opaque type RecipeInput <: Data = Data
+object RecipeInput extends RecipeInputGivens:
+  type Data = Seq[ItemWithCount]
+
+  inline def apply(data: Data): RecipeInput = data
+
+  extension(self: RecipeInput)
+    inline def value: Data = self
+
+  extension [F[_]](self: F[Data])
+    inline def mask: F[RecipeInput] = self
+
+trait RecipeInputGivens:
+  given Hash[RecipeInput] = Hash[Data].mask
+  given Show[RecipeInput] = Show[Data].mask
--- api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/RecipeOutput.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/RecipeOutput.scala
@@ -1,7 +1,21 @@
 package net.raystarkmc.craftingsimulator.domain.recipe
 
-import io.github.iltotore.iron.*
+import cats.*
+import cats.implicits.*
+import RecipeOutput.*
 
-type RecipeOutput = RecipeOutput.T
-object RecipeOutput
-    extends RefinedType[Seq[ItemWithCount], Pure]
+opaque type RecipeOutput <: Data = Data
+object RecipeOutput extends RecipeOutputGivens:
+  type Data = Seq[ItemWithCount]
+
+  inline def apply(data: Data): RecipeOutput = data
+
+  extension(self: RecipeOutput)
+    inline def value: Data = self
+
+  extension [F[_]](self: F[Data])
+    inline def mask: F[RecipeOutput] = self
+
+trait RecipeOutputGivens:
+  given Hash[RecipeOutput] = Hash[Data].mask
+  given Show[RecipeOutput] = Show[Data].mask
