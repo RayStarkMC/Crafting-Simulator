7e8ed399d58d3039b245e4406a6090d312a4b16a
RayStarkMC <raystarkmc@gmail.com>
2025-04-27 19:46:38 +0900

    ItemCountからIron解除

--- api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/ItemCount.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/ItemCount.scala
@@ -2,22 +2,29 @@ package net.raystarkmc.craftingsimulator.domain.recipe
 
 import cats.*
 import cats.data.*
-import cats.implicits.*
+import cats.syntax.all.*
+import cats.instances.all.given
 import cats.derived.*
-import io.github.iltotore.iron.*
-import io.github.iltotore.iron.constraint.all.*
 
-type ItemCount = ItemCount.T
-object ItemCount extends RefinedType[Long, Greater[0]]:
-  enum Failure derives Hash, Show:
+opaque type ItemCount = Long
+object ItemCount extends ItemCountGivens:
+  extension (self: ItemCount)
+    def value: Long = self
+
+  enum Failure derives Eq, Hash, Show:
     case IsNotGreaterThen0
 
   type AE[F[_]] = ApplicativeError[F, NonEmptyChain[Failure]]
 
-  def ae[F[_] : AE as F](value: Long): F[ItemCount] =
-    val checkGreaterThan0 = F.fromOption(
-      value.refineOption[Greater[0]],
-      NonEmptyChain.one(Failure.IsNotGreaterThen0)
-    )
-    ItemCount.assume(value).pure[F]
-      <* checkGreaterThan0
+  def ae[F[_] : AE as F](value: Long): F[ItemCount] = {
+    F.raiseWhen(value <= 0)(NonEmptyChain.one(Failure.IsNotGreaterThen0)) *>
+      F.pure(value)
+  }
+
+private inline def wrapItemCountF[F[_]](f: F[Long]): F[ItemCount] = f
+
+trait ItemCountGivens:
+  given Eq[ItemCount] = wrapItemCountF(Eq[Long])
+  given Hash[ItemCount] = wrapItemCountF(Hash[Long])
+  given Order[ItemCount] = wrapItemCountF(Order[Long])
+  given Show[ItemCount] = wrapItemCountF(Show[Long])
