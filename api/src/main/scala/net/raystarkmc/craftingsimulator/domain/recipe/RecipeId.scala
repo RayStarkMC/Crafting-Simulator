1bb27c80093a8b48154e2a4a99291a5d8902898b
RayStarkMC <raystarkmc@gmail.com>
2025-04-28 00:14:43 +0900

    RecipeIdからIronへの依存を除去

--- api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/RecipeId.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/RecipeId.scala
@@ -1,6 +1,23 @@
 package net.raystarkmc.craftingsimulator.domain.recipe
 
-import net.raystarkmc.craftingsimulator.lib.domain.*
+import cats.effect.std.UUIDGen
+import cats.{Eq, Hash, Order, Show}
 
-type RecipeId = RecipeId.T
-object RecipeId extends ModelIdUUID[RecipeContext]
+import java.util.UUID
+
+opaque type RecipeId = UUID
+object RecipeId extends RecipeIdGivens:
+  extension (self: RecipeId) def value: UUID = self
+
+  def apply(value: UUID): RecipeId = value
+
+  def generate[F[_] : UUIDGen]: F[RecipeId] =
+    wrapRecipeIdF(UUIDGen.randomUUID)
+
+private inline def wrapRecipeIdF[F[_]](f: F[UUID]): F[RecipeId] = f
+
+private trait RecipeIdGivens:
+  given Eq[RecipeId] = wrapRecipeIdF(Eq[UUID])
+  given Hash[RecipeId] = wrapRecipeIdF(Hash[UUID])
+  given Order[RecipeId] = wrapRecipeIdF(Order[UUID])
+  given Show[RecipeId] = wrapRecipeIdF(Show[UUID])
