b300a18a58224bf2bc536a120bac1edbd503ffe5
RayStarkMC <raystarkmc@gmail.com>
2025-04-29 18:32:51 +0900

    inParallel定義

--- api/src/main/scala/net/raystarkmc/craftingsimulator/lib/domain/ModelName.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/lib/domain/ModelName.scala
@@ -2,36 +2,42 @@ package net.raystarkmc.craftingsimulator.lib.domain
 
 import cats.*
 import cats.data.*
+import cats.instances.all.given
+import cats.syntax.all.*
 import cats.derived.*
-import cats.implicits.*
+import net.raystarkmc.craftingsimulator.lib.cats.*
 
 opaque type ModelName = String
 object ModelName extends ModelNameGivens:
-  extension(self: ModelName)
-    def value: String = self
+  extension (self: ModelName) def value: String = self
 
-  def unapply(s: ModelName): String = s
-
-  enum Failure derives Hash, Show:
+  enum Failure derives Eq, Hash, Show, Order:
     case IsBlank extends Failure
     case LengthExceeded extends Failure
     case ContainsControlCharacter extends Failure
 
-  def ae[F[_]: ([G[_]] =>> ApplicativeError[G, NonEmptyChain[Failure]]) as F](
-      value: String
-  ): F[ModelName] =
+  def ae[
+      F[_]: ApplicativeErrorWithNec[Failure] as F
+  ](value: String): F[ModelName] =
 
     val pattern = ".*\\p{Cntrl}.*".r
-    
-    F.raiseWhen(value.isBlank)(NonEmptyChain.one(Failure.IsBlank)) *> 
-      F.raiseWhen(value.length > 100)(NonEmptyChain.one(Failure.LengthExceeded)) *>
-      F.raiseWhen(pattern.findFirstIn(value).nonEmpty)(NonEmptyChain.one(Failure.ContainsControlCharacter)) *>
-      F.pure(value)
+    val maxLength = 100
 
-private inline def wrapF[F[_]](f: F[String]): F[ModelName] = f
+    F.pure(value)
+      <* F.raiseWhen(value.isBlank)(NonEmptyChain.one(Failure.IsBlank))
+      <* F.raiseWhen(value.length > maxLength)(NonEmptyChain.one(Failure.LengthExceeded))
+      <* F.raiseWhen(pattern.findFirstIn(value).nonEmpty)(NonEmptyChain.one(Failure.ContainsControlCharacter))
+
+  def inParallel[
+      M[_]: {Parallel as P, MonadErrorWithNec[Failure]}
+  ](value: String): M[ModelName] =
+    given ApplicativeError[P.F, NonEmptyChain[Failure]] = P.applicativeError
+    P.sequential(ae(value))
+
+private inline def wrapModelNameF[F[_]](f: F[String]): F[ModelName] = f
 
 private trait ModelNameGivens:
-  given Eq[ModelName] = wrapF(Eq[String])
-  given Hash[ModelName] = wrapF(Hash[String])
-  given Order[ModelName] = wrapF(Order[String])
-  given Show[ModelName] = wrapF(Show[String])
+  given Eq[ModelName] = wrapModelNameF(Eq[String])
+  given Hash[ModelName] = wrapModelNameF(Hash[String])
+  given Order[ModelName] = wrapModelNameF(Order[String])
+  given Show[ModelName] = wrapModelNameF(Show[String])
--- api/src/test/scala/net/raystarkmc/craftingsimulator/lib/domain/ModelNameTest.scala
+++ api/src/test/scala/net/raystarkmc/craftingsimulator/lib/domain/ModelNameTest.scala
@@ -4,33 +4,35 @@ import org.scalatest.freespec.AnyFreeSpec
 import cats.*
 import cats.implicits.*
 import cats.data.*
+import net.raystarkmc.craftingsimulator.lib.cats.*
+import net.raystarkmc.craftingsimulator.lib.domain.ModelName.Failure
 
 class ModelNameTest extends AnyFreeSpec:
   type F[A] = EitherNec[ModelName.Failure, A]
 
-  "制御文字が含まれる場合失敗" in :
+  "制御文字が含まれる場合失敗" in:
     val expected =
       ModelName.Failure.ContainsControlCharacter.asLeft[ModelName].toEitherNec
     val actual = ModelName.ae[F]("a\t")
 
     assert(expected eqv actual)
 
-  "空文字列の場合失敗" in :
+  "空文字列の場合失敗" in:
     val expected = ModelName.Failure.IsBlank.asLeft[ModelName].toEitherNec
     val actual = ModelName.ae[F]("")
     assert(expected eqv actual)
 
-  "空白のみの場合失敗" in :
+  "空白のみの場合失敗" in:
     val expected = ModelName.Failure.IsBlank.asLeft[ModelName].toEitherNec
     val actual = ModelName.ae[F](" ")
     assert(expected eqv actual)
 
-  "全角空白のみの場合失敗" in :
+  "全角空白のみの場合失敗" in:
     val expected = ModelName.Failure.IsBlank.asLeft[ModelName].toEitherNec
     val actual = ModelName.ae[F]("　")
     assert(expected eqv actual)
 
-  "日本語で生成" in :
+  "日本語で生成" in:
     val expected = "サンプルアイテム"
     val actual = ModelName.ae[F]("サンプルアイテム")
 
@@ -38,9 +40,22 @@ class ModelNameTest extends AnyFreeSpec:
       expected eqv actual.getOrElse[ModelName](fail()).value
     }
 
-  "空文字列を含んだ日本語で生成" in :
+  "空文字列を含んだ日本語で生成" in:
     val expected = "サンプル アイテム"
     val actual = ModelName.ae("サンプル アイテム")
     assert {
       expected eqv actual.getOrElse[ModelName](fail()).value
+    }
+
+  "inParallelにより長さ超過と制御文字列含みのエラーがまとめて返される" in:
+    val expected = NonEmptyChain(
+      Failure.LengthExceeded,
+      Failure.ContainsControlCharacter
+    )
+    val actual = ModelName.inParallel[EitherWithNec[ModelName.Failure]](
+      "\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    )
+
+    assert {
+      expected eqv actual.swap.getOrElse[NonEmptyChain[ModelName.Failure]](fail()).sorted
     }
