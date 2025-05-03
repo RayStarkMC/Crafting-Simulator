c5b34beb7ed0294f217b4560de55c2567bb6646d
RayStarkMC <raystarkmc@gmail.com>
2025-05-03 22:06:39 +0900

    リファクタリング: 可読性向上

--- api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/controller/item/UpdateItemController.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/controller/item/UpdateItemController.scala
@@ -1,14 +1,17 @@
 package net.raystarkmc.craftingsimulator.port.api.http4s.controller.item
 
+import cats.*
+import cats.data.*
+import cats.instances.all.given
+import cats.syntax.all.*
 import cats.effect.Concurrent
 import cats.effect.kernel.Async
-import cats.syntax.all.given
 import io.circe.generic.auto.given
 import net.raystarkmc.craftingsimulator.port.api.http4s.controller.item.UpdateItemController.RequestBody
 import net.raystarkmc.craftingsimulator.usecase.command.UpdateItemCommandHandler
-import net.raystarkmc.craftingsimulator.usecase.command.UpdateItemCommandHandler.*
 import org.http4s.*
 import org.http4s.circe.CirceEntityCodec.given
+import org.http4s.dsl.*
 
 trait UpdateItemController[F[_]]:
   def run: PartialFunction[Request[F], F[Response[F]]]
@@ -17,30 +20,25 @@ object UpdateItemController extends UpdateItemControllerGivens:
   case class RequestBody(name: String)
 
 trait UpdateItemControllerGivens:
-  given [F[_]: {Concurrent, UpdateItemCommandHandler}]
-    => UpdateItemController[F] =
-    object instance extends UpdateItemController[F]:
-      private val dsl = org.http4s.dsl.Http4sDsl[F]
-      import dsl.*
-      private val handler = summon[UpdateItemCommandHandler[F]]
+  given [F[_]: {UpdateItemCommandHandler as handler, Http4sDsl as dsl, Concurrent}] => UpdateItemController[F]:
+    import dsl.*
 
-      def run: PartialFunction[Request[F], F[Response[F]]] = {
-        case req @ PUT -> Root / "api" / "items" / UUIDVar(itemId) =>
-          for {
-            body <- req.as[RequestBody]
-            command = Command(
-              id = itemId,
-              name = body.name
-            )
-            result <- handler.run(command)
-            res <- result.fold(
-              error =>
-                error match {
-                  case Failure.ValidationFailed(detail) => BadRequest()
-                  case Failure.NotFound                 => NotFound()
-                },
-              _ => Ok()
-            )
-          } yield res
-      }
-    instance
+    def run: PartialFunction[Request[F], F[Response[F]]] = {
+      case req @ PUT -> Root / "api" / "items" / UUIDVar(itemId) =>
+        val eitherT = for {
+          body <- EitherT.right[UpdateItemCommandHandler.Failure] {
+            req.as[RequestBody]
+          }
+          command = UpdateItemCommandHandler.Command(
+            id = itemId,
+            name = body.name
+          )
+          _ <- EitherT(handler.run(command))
+          response <- EitherT.right[UpdateItemCommandHandler.Failure](Ok())
+        } yield response
+
+        eitherT.valueOrF {
+          case UpdateItemCommandHandler.Failure.ValidationFailed(detail) => BadRequest()
+          case UpdateItemCommandHandler.Failure.NotFound => NotFound()
+        }
+    }
