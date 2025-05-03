aea34df59bf7f9a3c9e618ac90266060d50f610a
RayStarkMC <raystarkmc@gmail.com>
2025-05-03 22:21:35 +0900

    リファクタリング: 可読性向上

--- api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/controller/item/SearchItemsController.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/controller/item/SearchItemsController.scala
@@ -10,29 +10,22 @@ import net.raystarkmc.craftingsimulator.usecase.query.SearchItemsQueryHandler
 import net.raystarkmc.craftingsimulator.usecase.query.SearchItemsQueryHandler.Input
 import org.http4s.*
 import org.http4s.circe.CirceEntityCodec.given
+import org.http4s.dsl.*
 
 trait SearchItemsController[F[_]]:
   def run: PartialFunction[Request[F], F[Response[F]]]
 
-object SearchItemsController extends SearchItemsControllerGivens {
-  case class RequestBody(
-      name: Option[String]
-  )
-}
+object SearchItemsController extends SearchItemsControllerGivens:
+  case class RequestBody(name: Option[String])
 
 trait SearchItemsControllerGivens:
-  given [F[_]: {Concurrent, SearchItemsQueryHandler}]
-    => SearchItemsController[F] =
-    object instance extends SearchItemsController[F]:
-      private val dsl = org.http4s.dsl.Http4sDsl[F]
-      import dsl.*
-      private val handler = summon[SearchItemsQueryHandler[F]]
+  given [F[_]: {SearchItemsQueryHandler as handler, Http4sDsl as dsl, Concurrent}] => SearchItemsController[F]:
+    import dsl.*
 
-      def run: PartialFunction[Request[F], F[Response[F]]] =
-        case req @ POST -> Root / "api" / "search" / "items" =>
-          for {
-            body <- req.as[RequestBody]
-            queryModel <- handler.run(Input(name = body.name))
-            res <- Ok(queryModel.asJson)
-          } yield res
-    instance
+    def run: PartialFunction[Request[F], F[Response[F]]] =
+      case req @ POST -> Root / "api" / "search" / "items" =>
+        for {
+          body <- req.as[RequestBody]
+          queryModel <- handler.run(Input(name = body.name))
+          res <- Ok(queryModel.asJson)
+        } yield res
