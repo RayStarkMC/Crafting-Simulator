57e8bcc3e1dd46c2f63a0cc6f47e96b29986aa11
RayStarkMC <raystarkmc@gmail.com>
2025-05-03 19:38:36 +0900

    リファクタリング: インターフェース簡略化

--- api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/ItemCount.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/domain/recipe/ItemCount.scala
@@ -5,6 +5,7 @@ import cats.data.*
 import cats.derived.*
 import cats.instances.all.given
 import cats.syntax.all.*
+import net.raystarkmc.craftingsimulator.lib.cats.*
 
 opaque type ItemCount = Long
 object ItemCount extends ItemCountGivens:
@@ -14,7 +15,7 @@ object ItemCount extends ItemCountGivens:
   enum Failure derives Eq, Hash, Show:
     case IsNotGreaterThen0
 
-  def ae[F[_] : ([G[_]] =>> ApplicativeError[G, NonEmptyChain[Failure]]) as F](value: Long): F[ItemCount] =
+  def ae[F[_] : ApplicativeErrorWithNec[Failure] as F](value: Long): F[ItemCount] =
     F.raiseWhen(value <= 0)(NonEmptyChain.one(Failure.IsNotGreaterThen0)) *>
       F.pure(value)
 
