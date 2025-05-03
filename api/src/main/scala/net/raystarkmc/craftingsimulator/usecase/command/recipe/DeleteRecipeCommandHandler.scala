bba0f357838ff065b00817facf5d36fb516e8da8
RayStarkMC <raystarkmc@gmail.com>
2025-05-04 04:13:11 +0900

    リファクタリング: 不要なTrait削除

--- api/src/main/scala/net/raystarkmc/craftingsimulator/usecase/command/recipe/DeleteRecipeCommandHandler.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/usecase/command/recipe/DeleteRecipeCommandHandler.scala
@@ -13,18 +13,17 @@ import java.util.UUID
 trait DeleteRecipeCommandHandler[F[_]]:
   def run(command: Command): F[Output]
 
-object DeleteRecipeCommandHandler extends DeleteRecipeCommandHandlerGivens:
+object DeleteRecipeCommandHandler:
   case class Command(id: UUID) derives Hash, Show
   case class Output() derives Hash, Show
 
-trait DeleteRecipeCommandHandlerGivens:
-  given [F[_]: {Monad, UUIDGen, RecipeRepository}]
-    => DeleteRecipeCommandHandler[F] =
+  given [F[_] : {Monad, UUIDGen, RecipeRepository}]
+  => DeleteRecipeCommandHandler[F] =
     object instance extends DeleteRecipeCommandHandler[F]:
       private val recipeRepository = summon[RecipeRepository[F]]
 
       def run(
-          command: Command
+        command: Command
       ): F[Output] =
         val recipeId = RecipeId(command.id)
         for {
