a4c1ab3601af50d37013e83238e545c5b01a8cbf
RayStarkMC <raystarkmc@gmail.com>
2025-05-03 21:01:37 +0900

    リファクタリング: dslをimport

--- api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/controller/item/DeleteItemController.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/controller/item/DeleteItemController.scala
@@ -7,6 +7,7 @@ import cats.instances.all.given
 import cats.effect.Concurrent
 import net.raystarkmc.craftingsimulator.usecase.command.DeleteItemCommandHandler
 import org.http4s.*
+import org.http4s.dsl.*
 
 trait DeleteItemController[F[_]]:
   def run: PartialFunction[Request[F], F[Response[F]]]
@@ -15,7 +16,7 @@ object DeleteItemController extends DeleteItemControllerGivens
 
 trait DeleteItemControllerGivens:
   given [F[_]: {Concurrent, DeleteItemCommandHandler as handler}] => DeleteItemController[F]:
-    private val dsl = org.http4s.dsl.Http4sDsl[F]
+    private val dsl = Http4sDsl[F]
     import dsl.*
 
     def run: PartialFunction[Request[F], F[Response[F]]] =
