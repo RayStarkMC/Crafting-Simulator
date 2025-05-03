863c7f7f2c99f71819f1cabf450790707457c506
RayStarkMC <raystarkmc@gmail.com>
2025-05-04 04:58:49 +0900

    トランザクションを考慮するように変更

--- api/src/main/scala/net/raystarkmc/craftingsimulator/usecase/command/recipe/DeleteRecipeCommandHandler.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/usecase/command/recipe/DeleteRecipeCommandHandler.scala
@@ -1,29 +1,40 @@
 package net.raystarkmc.craftingsimulator.usecase.command.recipe
 
 import cats.*
+import cats.data.*
 import cats.derived.*
 import cats.effect.std.UUIDGen
 import cats.instances.all.given
-import cats.syntax.all.given
+import cats.syntax.all.*
 import net.raystarkmc.craftingsimulator.domain.recipe.*
+import net.raystarkmc.craftingsimulator.lib.transaction.Transaction
 import net.raystarkmc.craftingsimulator.usecase.command.recipe.DeleteRecipeCommandHandler.*
 
 import java.util.UUID
 
 trait DeleteRecipeCommandHandler[F[_]]:
-  def run(command: Command): F[Unit]
+  def run(command: Command): F[Either[Failure, Unit]]
 
 object DeleteRecipeCommandHandler:
   case class Command(id: UUID) derives Eq, Hash, Order, Show
+  enum Failure derives Eq, Hash, Order, Show:
+    case ModelNotFound
 
-  given [F[_]: {Monad, UUIDGen, RecipeRepository as recipeRepository}] => DeleteRecipeCommandHandler[F]:
-    def run(command: Command): F[Unit] =
+  given [
+      F[_],
+      G[_]: {RecipeRepository as recipeRepository, Monad}
+  ] => (T: Transaction[G, F]) => DeleteRecipeCommandHandler[F]:
+    def run(command: Command): F[Either[Failure, Unit]] =
       val recipeId = RecipeId(command.id)
-      for {
-        recipeOpt <- recipeRepository.resolveById(recipeId)
-        _ <- recipeOpt.fold {
-          Applicative[F].unit
-        } { item =>
-          recipeRepository.delete(item)
-        }
-      } yield ()
+      val eitherT = T.withTransaction {
+        for {
+          recipe <- EitherT.fromOptionF(
+            recipeRepository.resolveById(recipeId),
+            Failure.ModelNotFound
+          )
+          _ <- EitherT.right[Failure] {
+            recipeRepository.delete(recipe)
+          }
+        } yield ()
+      }
+      eitherT.value
