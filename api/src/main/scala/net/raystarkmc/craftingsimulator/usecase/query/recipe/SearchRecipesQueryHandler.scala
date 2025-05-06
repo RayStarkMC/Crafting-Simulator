71d462185ab9ac382c094c74e580a2456e7ad60a
RayStarkMC <raystarkmc@gmail.com>
2025-05-06 19:11:42 +0900

    SearchRecipesQueryHandler定義

---
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/usecase/query/recipe/SearchRecipesQueryHandler.scala
@@ -0,0 +1,20 @@
+package net.raystarkmc.craftingsimulator.usecase.query.recipe
+
+import cats.*
+import cats.data.*
+import cats.instances.all.given
+import cats.syntax.all.*
+import java.util.UUID
+import net.raystarkmc.craftingsimulator.usecase.query.recipe.SearchRecipesQueryHandler.*
+
+trait SearchRecipesQueryHandler[F[_]]:
+  def run(input: Input): F[Recipes]
+
+object SearchRecipesQueryHandler:
+  case class Input(
+    name: Option[String],
+    inputIds: Option[NonEmptySet[UUID]],
+    outputIds: Option[NonEmptySet[UUID]],
+  )
+  case class Recipes(recipes: List[Recipe])
+  case class Recipe(id: UUID, name: String)
