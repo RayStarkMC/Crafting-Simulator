8c10f41977fa51b90a88502c153039f73e1a5a04
RayStarkMC <raystarkmc@gmail.com>
2025-05-14 22:00:42 +0900

    リファクタリングtestYoloを移動

--- api/src/test/scala/net/raystarkmc/craftingsimulator/port/db/doobie/postgres/LocalDBTestEnvironment.scala
+++ api/src/test/scala/net/raystarkmc/craftingsimulator/port/db/doobie/postgres/LocalDBTestEnvironment.scala
@@ -2,10 +2,10 @@ package net.raystarkmc.craftingsimulator.port.db.doobie.postgres
 
 import cats.*
 import cats.data.*
-import cats.syntax.all.*
-import cats.instances.all.given
 import cats.effect.Async
 import cats.effect.IO
+import cats.instances.all.given
+import cats.syntax.all.*
 import doobie.*
 import doobie.implicits.*
 import doobie.util.transactor.Transactor
@@ -13,20 +13,19 @@ import doobie.util.yolo.Yolo
 import net.raystarkmc.craftingsimulator.lib.transaction.Transaction
 
 trait LocalDBTestEnvironment extends Transaction.Noop:
-  export testYolo.*
-
-object LocalDBTestEnvironment extends LocalDBTestEnvironment
-
-val testYolo = Transactor.after
-  .set(
-    Transactor
-      .fromDriverManager[IO](
+  val testYolo: Yolo[IO] = Transactor.after
+    .set(
+      Transactor.fromDriverManager[IO](
         driver = "org.postgresql.Driver",
         url = "jdbc:postgresql://localhost:5432/crafting_simulator",
         user = "admin",
         password = "admin",
         logHandler = LogHandler.jdkLogHandler[IO].some,
       ),
-    FC.rollback,
-  )
-  .yolo
+      FC.rollback,
+    )
+    .yolo
+
+  export testYolo.*
+
+object LocalDBTestEnvironment extends LocalDBTestEnvironment
