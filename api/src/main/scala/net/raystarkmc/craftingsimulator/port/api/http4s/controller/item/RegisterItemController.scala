85043cacf1ea6fe6cb09986f38cd02553e5ffca6
RayStarkMC <raystarkmc@gmail.com>
2025-05-14 21:06:27 +0900

    リファクタリング

--- api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/controller/item/RegisterItemController.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/controller/item/RegisterItemController.scala
@@ -17,18 +17,17 @@ trait RegisterItemController[F[_]]:
 
 object RegisterItemController:
   given [F[_]: {RegisterItemCommandHandler as handler, Http4sDsl as dsl, Concurrent}] => RegisterItemController[F]:
-
     import dsl.*
 
     def run: PartialFunction[Request[F], F[Response[F]]] =
       case req @ POST -> Root / "api" / "items" =>
-        val eitherT = for {
-          command <- EitherT.right[RegisterItemCommandHandler.Failure] {
-            req.as[RegisterItemCommandHandler.Command]
-          }
-          RegisterItemCommandHandler.Output(id) <- EitherT(handler.run(command))
-          res <- EitherT.right[RegisterItemCommandHandler.Failure] {
-            Ok(id.show)
-          }
-        } yield res
+        val eitherT =
+          for
+            command <- EitherT.right[RegisterItemCommandHandler.Failure]:
+              req.as[RegisterItemCommandHandler.Command]
+            RegisterItemCommandHandler.Output(id) <- EitherT:
+              handler.run(command)
+            res <- EitherT.right[RegisterItemCommandHandler.Failure]:
+              Ok(id.show)
+          yield res
         eitherT.leftMap(_.show).valueOrF(BadRequest(_))
