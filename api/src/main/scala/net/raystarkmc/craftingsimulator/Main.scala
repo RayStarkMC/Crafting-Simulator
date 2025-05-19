21d7f377debcbe17e6e806085b1ebf77af2ac4b6
RayStarkMC <raystarkmc@gmail.com>
2025-05-20 00:30:40 +0900

    Routeファイル外だし

--- api/src/main/scala/net/raystarkmc/craftingsimulator/Main.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/Main.scala
@@ -9,6 +9,7 @@ import com.comcast.ip4s.port
 import net.raystarkmc.craftingsimulator.port.api.http4s.controller.*
 import net.raystarkmc.craftingsimulator.port.api.http4s.controller.item.*
 import net.raystarkmc.craftingsimulator.port.api.http4s.controller.recipe.*
+import net.raystarkmc.craftingsimulator.port.api.http4s.routes.allRoutes
 import net.raystarkmc.craftingsimulator.port.db.doobie.postgres.instances.given
 import org.http4s.HttpRoutes
 import org.http4s.Response
@@ -19,22 +20,6 @@ import org.http4s.server.middleware.ErrorHandling
 import scala.concurrent.duration.*
 
 given [F[_]] => Http4sDsl[F] = Http4sDsl[F]
-
-def allRoutes[
-  F[_]: {Monad, RegisterItemController as RegisterItemController, SearchItemsController as SearchItemsController,
-    GetItemController as GetItemController, UpdateItemController as UpdateItemController,
-    DeleteItemController as DeleteItemController, RegisterRecipeController as RegisterRecipeController}
-]: HttpRoutes[F] =
-  HttpRoutes.empty[F]
-    <+> RegisterRecipeController.route
-    <+> HttpRoutes.of[F](
-      PartialFunction.empty
-        orElse RegisterItemController.run
-        orElse SearchItemsController.run
-        orElse GetItemController.run
-        orElse UpdateItemController.run
-        orElse DeleteItemController.run
-    )
 
 object Main extends IOApp:
   def run(args: List[String]): IO[ExitCode] =
