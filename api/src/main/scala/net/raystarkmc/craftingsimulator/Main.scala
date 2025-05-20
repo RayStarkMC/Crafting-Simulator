6db4e434a2ee33c82c8d959c51e32e7fc23744eb
RayStarkMC <raystarkmc@gmail.com>
2025-05-21 02:06:27 +0900

    ルーティングモジュールを独立。

--- api/src/main/scala/net/raystarkmc/craftingsimulator/Main.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/Main.scala
@@ -4,22 +4,11 @@ import cats.*
 import cats.effect.*
 import cats.instances.all.given
 import cats.syntax.all.*
-import com.comcast.ip4s.ipv4
-import com.comcast.ip4s.port
-import net.raystarkmc.craftingsimulator.port.api.http4s.controller.*
-import net.raystarkmc.craftingsimulator.port.api.http4s.controller.item.*
-import net.raystarkmc.craftingsimulator.port.api.http4s.controller.recipe.*
+import com.comcast.ip4s.*
 import net.raystarkmc.craftingsimulator.port.api.http4s.routes.allRoutes
-import net.raystarkmc.craftingsimulator.port.db.doobie.postgres.instances.given
-import org.http4s.HttpRoutes
-import org.http4s.Response
-import org.http4s.dsl.*
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.implicits.*
 import org.http4s.server.middleware.ErrorHandling
 import scala.concurrent.duration.*
-
-given [F[_]] => Http4sDsl[F] = Http4sDsl[F]
 
 object Main extends IOApp:
   def run(args: List[String]): IO[ExitCode] =
--- api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/routes/Route.scala
+++ api/src/main/scala/net/raystarkmc/craftingsimulator/port/api/http4s/routes/Route.scala
@@ -1,28 +1,30 @@
 package net.raystarkmc.craftingsimulator.port.api.http4s.routes
 
 import cats.*
+import cats.effect.*
+import cats.effect.std.*
 import cats.instances.all.given
 import cats.syntax.all.*
+import net.raystarkmc.craftingsimulator.port.api.http4s.controller.*
 import net.raystarkmc.craftingsimulator.port.api.http4s.controller.item.*
 import net.raystarkmc.craftingsimulator.port.api.http4s.controller.recipe.*
+import net.raystarkmc.craftingsimulator.port.api.http4s.routes.allRoutes
+import net.raystarkmc.craftingsimulator.port.db.doobie.postgres.instances.given
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
-def allRoutes[
-  F[_]: {Monad, RegisterItemController as RegisterItemController, SearchItemsController as SearchItemsController,
-    GetItemController as GetItemController, UpdateItemController as UpdateItemController,
-    DeleteItemController as DeleteItemController, RegisterRecipeController as RegisterRecipeController,
-    SearchRecipesController as SearchRecipesController, UpdateRecipeController as UpdateRecipeController}
-]: HttpRoutes[F] =
+given [F[_]] => Http4sDsl[F] = Http4sDsl[F]
+
+def allRoutes[F[_]: Async]: HttpRoutes[F] =
   HttpRoutes.empty[F]
-    <+> RegisterRecipeController.route
-    <+> SearchRecipesController.route
-    <+> UpdateRecipeController.route
+    <+> summon[RegisterRecipeController[F]].route
+    <+> summon[SearchRecipesController[F]].route
+    <+> summon[UpdateRecipeController[F]].route
     <+> HttpRoutes.of[F](
       PartialFunction.empty
-        orElse RegisterItemController.run
-        orElse SearchItemsController.run
-        orElse GetItemController.run
-        orElse UpdateItemController.run
-        orElse DeleteItemController.run
+        orElse summon[RegisterItemController[F]].run
+        orElse summon[SearchItemsController[F]].run
+        orElse summon[GetItemController[F]].run
+        orElse summon[UpdateItemController[F]].run
+        orElse summon[DeleteItemController[F]].run
     )
